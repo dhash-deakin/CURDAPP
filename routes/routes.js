cd0f809468445b146fdab2353efb17845b01221e
Dilum B <s220459698@deakin.edu.au>
2023-09-18 19:31:36 +1000

    edit and delete code added

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/users");
 const multer = require("multer");
+const fs = require('fs');
 
 //image upload
 var storage = multer.diskStorage({
@@ -63,8 +64,106 @@ router.get("/", async (req, res) => {
     }
   });
 
+// Edit a user route
+router.get("/edit/:id", async (req, res) => {
+    try {
+      const id = req.params.id;
+      const user = await User.findById(id);
+  
+      if (!user) {
+        return res.redirect('/');
+      }
+  
+      res.render('edit_user', {
+        title: "Edit User",
+        user: user
+      });
+    } catch (err) {
+      console.error(err);
+      res.redirect('/');
+    }
+  });  
+
+// Update a user route
+router.post('/update/:id', upload, async (req, res) => {
+    try {
+      const id = req.params.id;
+      let newImage = '';
+  
+      if (req.file) {
+        newImage = req.file.filename;
+  
+        // Delete the old image if it exists
+        try {
+          fs.unlinkSync('./uploads/' + req.body.old_image);
+        } catch (err) {
+          console.log(err);
+        }
+      } else {
+        newImage = req.body.old_image;
+      }
+  
+      const updatedUserData = {
+        name: req.body.name,
+        email: req.body.email,
+        phone: req.body.phone,
+        image: newImage,
+      };
+  
+      const user = await User.findByIdAndUpdate(id, updatedUserData);
+  
+      if (!user) {
+        return res.json({ message: 'User not found', type: 'danger' });
+      }
+  
+      req.session.message = {
+        type: 'success',
+        message: 'User updated successfully',
+      };
+  
+      res.redirect('/');
+    } catch (err) {
+      console.error(err);
+      res.json({ message: err.message, type: 'danger' });
+    }
+  });
+
+
+// Delete a user route
+router.get('/delete/:id', async (req, res) => {
+    try {
+      const id = req.params.id;
+  
+      // Find the user by ID
+      const user = await User.findById(id);
+  
+      if (!user) {
+        return res.json({ message: 'User not found', type: 'danger' });
+      }
+  
+      // Delete the user's image (if it exists)
+      if (user.image) {
+        fs.unlinkSync('./uploads/' + user.image);
+      }
+  
+      // Delete the user from the database
+      await User.findByIdAndRemove(id);
+  
+      req.session.message = {
+        type: 'success',
+        message: 'User deleted successfully',
+      };
+  
+      res.redirect('/');
+    } catch (err) {
+      console.error(err);
+      res.json({ message: err.message, type: 'danger' });
+    }
+  });
+  
+
 router.get("/add", (req, res) => {
   res.render("add_users", { title: "Add Users" });
 });
-
+ 
 module.exports = router;
